633331d34f27c7516346a8bcefb36ad24bf48857
Ibrahim Matar <mataribrahim23@gmail.com>
2019-01-27 19:10:22 -0600

    Initial commit

---
+++ Assets/Scripts/doorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorScript : MonoBehaviour
+{
+    private Animator _animator;
+    GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _animator.SetBool("open", true);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
